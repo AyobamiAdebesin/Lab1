5161e93020ea2533e8ca5d8607b8951caaeba7e2
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-02 03:26:09 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ayobami, I am in Backend of Wella Schools");
+            int nairaToDollar = 123;
+            double x= 3.2;
+            int nairaToDollarInt = (int)x;
+
+            Console.WriteLine($"Text with {nairaToDollar} and {x}");
+            Console.WriteLine($"Text with {nairaToDollarInt} and {x}");
+            
+            
+        }
+    }
+}
