df6067681130880b2c65a1a36f7e72997154fe8e
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-02 04:21:56 +0100

    Added new changes

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Ayobami, I am in Backend of Wella Schools");
-            int nairaToDollar = 123;
-            double x= 3.2;
-            int nairaToDollarInt = (int)x;
-
-            Console.WriteLine($"Text with {nairaToDollar} and {x}");
-            Console.WriteLine($"Text with {nairaToDollarInt} and {x}");
+            Console.WriteLine("I am learning Backend Development because I have a knack for solving problems");
+            
             
             
         }
